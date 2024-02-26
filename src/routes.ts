56546e84f3ba54778ebd574cc009e218a11a7ef0
Piotr Świderski <psswiderski@gmail.com>
2024-02-26 13:47:32 +0100

    add missing routes and validating middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,58 @@
-import { Express } from 'express';
+import { Express } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validateResource from "./middleware/validateResource";
+import { createUserSchema } from "./schema/user.schema";
+import {
+  createUserSessionHandler,
+  deleteSessionHandler,
+  getUserSessionHandler,
+} from "./controller/session.controller";
+import { createSessionSchema } from "./schema/session.schema";
+import requireUser from "./middleware/requireUser";
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from "./schema/product.schema";
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "./controller/product.controller";
+
 function routes(app: Express) {
-    
+  app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+  app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
+
+  app.get("/api/sessions", requireUser, getUserSessionHandler);
+  app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+  app.post(
+    "/api/products",
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+  app.put(
+    "/api/products",
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+  app.get(
+    "/api/products/:productId",
+    validateResource(getProductSchema),
+    getProductHandler
+  );
+  app.delete(
+    "/api/products/:productId",
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler
+  );
 }
 
 export default routes;

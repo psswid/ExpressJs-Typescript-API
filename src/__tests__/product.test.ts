998d54630bc7f9d97a763cc9e9508e82363789a9
Piotr Świderski <psswiderski@gmail.com>
2024-02-27 13:14:48 +0100

    add user auth checking tests

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -3,6 +3,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import createServer from "../utils/server";
 import mongoose from "mongoose";
 import { createProduct } from "../service/product.service";
+import { signJwt } from "../utils/jwt.utils";
 
 const app = createServer();
 
@@ -11,9 +12,16 @@ const userId = new mongoose.Types.ObjectId().toString();
 export const productPayload = {
   user: userId,
   title: "Title of test product",
-  description: "Description of test product",
+  description:
+    "Description of test product Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
   price: 99.0,
   image: "https://i.imgur.com/test.jpg",
+};
+
+export const userPayload = {
+  _id: userId,
+  email: "testuser@example.com",
+  name: "Test user",
 };
 
 describe("product", () => {
@@ -48,6 +56,41 @@ describe("product", () => {
         );
         expect(statusCode).toBe(200);
         expect(body.productId).toBe(product.productId);
+      });
+    });
+  });
+
+  describe("create product route", () => {
+    describe("given the user is not logged in", () => {
+      it("should return a 403", async () => {
+        const { statusCode } = await supertest(app).post("/api/products");
+
+        expect(statusCode).toBe(403);
+      });
+    });
+    describe("given the user is logged in", () => {
+      it("should return a 200 and create the product", async () => {
+        const jwt = signJwt(userPayload);
+
+        const { statusCode, body } = await supertest(app)
+          .post("/api/products")
+          .set("Authorization", `Bearer ${jwt}`)
+          .send(productPayload);
+
+        expect(statusCode).toBe(200);
+        expect(body).toStrictEqual({
+          __v: 0,
+          _id: expect.any(String),
+          createdAt: expect.any(String),
+          description:
+            "Description of test product Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+          image: "https://i.imgur.com/test.jpg",
+          price: 99,
+          productId: expect.any(String),
+          title: "Title of test product",
+          updatedAt: expect.any(String),
+          user: expect.any(String),
+        });
       });
     });
   });
